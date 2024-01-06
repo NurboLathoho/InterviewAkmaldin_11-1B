8127e62575a2d950f7015c031a7b7465654b3f06
Akmalidin <akmalmadakimov6@gmail.com>
2024-01-06 22:04:06 +0600

    Akmalidin

--- django_practice/prac2/apps/base/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Settings
-# Register your models here.
-@admin.register(Settings)
--- django_practice/rest/apps/base/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Settings
-# Register your models here.
-@admin.register(Settings)
-class SettingsAdmin(admin.ModelAdmin):
-    list_display = ['title', 'desc', 'image', 'price']
-    list_filter = ['title', 'price']
-    
-    prepopulated_fields = {'slug': ('title', )}
